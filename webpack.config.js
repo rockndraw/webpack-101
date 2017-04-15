1be6e25e71b2e235485887807a9862364d277123
LubinetsKN <lubinetskn@gmail.com>
2017-04-15 08:48:38 +0300

    add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: 'dist',
     filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'sass-loader'],
+          publicPath: '/dist',
+        }),
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -14,6 +27,11 @@ module.exports = {
       },
       hash: true,
       template: './src/index.ejs',
+    }),
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      disabled: false,
+      allChunks: true,
     }),
   ],
 };
