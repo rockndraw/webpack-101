b9b355db9d221755aa7c0dd49e903c2e5acf07c3
LubinetsKN <lubinetskn@gmail.com>
2017-04-15 09:43:27 +0300

    git ignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: 'dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: ['css-loader', 'sass-loader'],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
           publicPath: '/dist',
         }),
       },
